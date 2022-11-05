6b67a7b2c06862bc7a2da10fd42ca94191588f71
Douglas Hoffmann <douglashoffmann@outlook.es>
2022-11-05 09:10:43 -0300

    feat: initia component for PaymentModal

--- src/components/forms/PaymentModal.tsx
+++ src/components/forms/PaymentModal.tsx
@@ -1,4 +1,13 @@
 export interface Props {
   isOpen: boolean;
   onClose: () => void;
+  entityId: number;
 }
+
+const PaymentModal: React.FunctionComponent<Props> = ({
+  isOpen,
+  onClose,
+  entityId,
+}) => {};
+
+export default PaymentModal;
