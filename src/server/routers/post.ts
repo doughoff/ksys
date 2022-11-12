c84154aa52b83b0f58cc649f30d74a41f576db3a
Douglas Hoffmann <douglashoffmann@outlook.es>
2022-11-12 14:22:36 -0300

    feat: spacing configurations

--- src/server/routers/post.ts
+++ src/server/routers/post.ts
@@ -14,90 +14,90 @@ import { prisma } from '~/server/prisma';
  * @see https://github.com/prisma/prisma/issues/9353
  */
 const defaultPostSelect = Prisma.validator<Prisma.PostSelect>()({
-  id: true,
-  title: true,
-  text: true,
-  createdAt: true,
-  updatedAt: true,
+   id: true,
+   title: true,
+   text: true,
+   createdAt: true,
+   updatedAt: true,
 });
 
 export const postRouter = t.router({
-  list: t.procedure
-    .input(
-      z.object({
-        text: z.string().optional(),
-        pagination: z.object({
-          page: z.number().min(1).default(1),
-          pageSize: z.number().min(1).max(100).default(10),
-        }),
-      }),
-    )
-    .query(async ({ input }) => {
-      const { page, pageSize } = input.pagination;
-      const count = await prisma.post.count({
-        where: {
-          text: {
-            contains: input.text,
-          },
-        },
-      });
-      const items = await prisma.post.findMany({
-        select: defaultPostSelect,
-        where: {
-          text: {
-            contains: input.text,
-          },
-        },
-        take: pageSize + 1,
-        skip: (page - 1) * pageSize,
-        orderBy: {
-          createdAt: 'asc',
-        },
-      });
+   list: t.procedure
+      .input(
+         z.object({
+            text: z.string().optional(),
+            pagination: z.object({
+               page: z.number().min(1).default(1),
+               pageSize: z.number().min(1).max(100).default(10),
+            }),
+         }),
+      )
+      .query(async ({ input }) => {
+         const { page, pageSize } = input.pagination;
+         const count = await prisma.post.count({
+            where: {
+               text: {
+                  contains: input.text,
+               },
+            },
+         });
+         const items = await prisma.post.findMany({
+            select: defaultPostSelect,
+            where: {
+               text: {
+                  contains: input.text,
+               },
+            },
+            take: pageSize + 1,
+            skip: (page - 1) * pageSize,
+            orderBy: {
+               createdAt: 'asc',
+            },
+         });
 
-      return {
-        items: items,
-        count: count,
-      };
-    }),
-  byId: t.procedure
-    .input(
-      z.object({
-        id: z.string(),
+         return {
+            items: items,
+            count: count,
+         };
       }),
-    )
-    .query(async ({ input }) => {
-      const { id } = input;
-      const post = await prisma.post.findUnique({
-        where: { id },
-        select: defaultPostSelect,
-      });
-      if (!post) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: `No post with id '${id}'`,
-        });
-      }
-      return post;
-    }),
-  add: t.procedure
-    .input(
-      z.object({
-        id: z.string().uuid().optional(),
-        title: z.string().min(1).max(32),
-        text: z.string().min(1),
+   byId: t.procedure
+      .input(
+         z.object({
+            id: z.string(),
+         }),
+      )
+      .query(async ({ input }) => {
+         const { id } = input;
+         const post = await prisma.post.findUnique({
+            where: { id },
+            select: defaultPostSelect,
+         });
+         if (!post) {
+            throw new TRPCError({
+               code: 'NOT_FOUND',
+               message: `No post with id '${id}'`,
+            });
+         }
+         return post;
       }),
-    )
-    .mutation(async ({ input }) => {
-      // fake longe running operation
-      return prisma.$transaction(async (prisma) => {
-        await new Promise((r) => setTimeout(r, 2000));
+   add: t.procedure
+      .input(
+         z.object({
+            id: z.string().uuid().optional(),
+            title: z.string().min(1).max(32),
+            text: z.string().min(1),
+         }),
+      )
+      .mutation(async ({ input }) => {
+         // fake longe running operation
+         return prisma.$transaction(async (prisma) => {
+            await new Promise((r) => setTimeout(r, 2000));
 
-        const post = await prisma.post.create({
-          data: input,
-          select: defaultPostSelect,
-        });
-        return post;
-      });
-    }),
+            const post = await prisma.post.create({
+               data: input,
+               select: defaultPostSelect,
+            });
+            return post;
+         });
+      }),
 });
